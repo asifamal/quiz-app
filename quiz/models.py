5b182d4aa6ee17836ae0b7f181074b3fef4beb52
asifamal <asifamal6@gmail.com>
2025-11-12 19:40:08 +0530

    add custom User model with role field (Admin/User) and also aded superuser

---
+++ quiz/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    """Custom user model with role choices."""
+
+    ADMIN = 'ADMIN'
+    USER = 'USER'
+
+    ROLE_CHOICES = [
+        (ADMIN, 'Admin'),
+        (USER, 'User'),
+    ]
+
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default=USER)
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
--- quizapp/settings.py
+++ quizapp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'quiz.apps.QuizConfig',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Use custom user model from quiz app
+AUTH_USER_MODEL = 'quiz.User'
